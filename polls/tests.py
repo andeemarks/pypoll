26b62a41505c9f7f6be1249f912b50b4c9e2fe0e
Andy Marks <amarks@thoughtworks.com>
2012-07-24 16:56:11 +0800

    Added basic tests for GET requests

--- polls/tests.py
+++ polls/tests.py
@@ -1,22 +1,41 @@
-from django.utils import unittest
+from django.utils import unittest, timezone
 from polls.models import Poll
 import datetime
-from django.utils import timezone
+from django.test import Client
 
 class PollTestCase(unittest.TestCase):
-    # def setUp(self):
+  def setUp(self):
+    self.poll = Poll.objects.create(question="new", pub_date=timezone.now())
 
-    def test_new_polls_were_published_recently(self):
-        """Poll less than 1 day old are considered to have been published recently"""
-        self.new_poll = Poll.objects.create(question="new", pub_date=timezone.now())
-        self.assertEqual(self.new_poll.was_published_recently(), True)
+  def test_new_polls_were_published_recently(self):
+    """Poll less than 1 day old are considered to have been published recently"""
+    self.assertTrue(self.poll.was_published_recently())
 
-    def test_old_polls_were_not_published_recently(self):
-        """Poll more than 1 day old are not considered to have been published recently"""
-        self.old_poll = Poll.objects.create(question="new", pub_date=timezone.now() - datetime.timedelta(days=3))
-        self.assertEqual(self.old_poll.was_published_recently(), False)
+  def test_old_polls_were_not_published_recently(self):
+    """Poll more => 1 day old are not considered to have been published recently"""
+    self.poll.pub_date += datetime.timedelta(days=-1)
+    self.assertFalse(self.poll.was_published_recently())
 
-    def test_1_day_old_polls_were_published_recently(self):
-        """Poll exactly 1 day old are considered to have been published recently"""
-        self.old_poll = Poll.objects.create(question="new", pub_date=timezone.now() - datetime.timedelta(days=1))
-        self.assertEqual(self.old_poll.was_published_recently(), True)
+  def test_1_day_old_polls_were_published_recently(self):
+    """Poll < 1 day old are considered to have been published recently"""
+    self.poll.pub_date += datetime.timedelta(hours=-23, minutes=-59, seconds=-59)
+    self.assertTrue(self.poll.was_published_recently())
+
+class ViewPollsTestCase(unittest.TestCase):
+  def setUp(self):
+    self.c = Client()
+
+  def test_access_to_list_polls_page(self):
+    """Main entry point to app is responsive"""
+    response = self.c.get('/polls/')
+    self.assertEqual(response.status_code, 200)
+
+  def test_access_to_specific_polls_page(self):
+    """Poll entry point is responsive"""
+    response = self.c.get('/polls/1/')
+    self.assertEqual(response.status_code, 200)
+
+  def test_access_to_admin_page(self):
+    """Admin entry point is responsive"""
+    response = self.c.get('/admin/')
+    self.assertEqual(response.status_code, 200)
