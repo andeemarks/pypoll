de26426390f1d20790670376cb24f8baf9ec0cf8
Andy Marks <amarks@thoughtworks.com>
2012-07-24 16:16:59 +0800

    Initial take at unit tests for Poll model

--- polls/tests.py
+++ polls/tests.py
@@ -1,16 +1,22 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from django.utils import unittest
+from polls.models import Poll
+import datetime
+from django.utils import timezone
 
-Replace this with more appropriate tests for your application.
-"""
+class PollTestCase(unittest.TestCase):
+    # def setUp(self):
 
-from django.test import TestCase
+    def test_new_polls_were_published_recently(self):
+        """Poll less than 1 day old are considered to have been published recently"""
+        self.new_poll = Poll.objects.create(question="new", pub_date=timezone.now())
+        self.assertEqual(self.new_poll.was_published_recently(), True)
 
+    def test_old_polls_were_not_published_recently(self):
+        """Poll more than 1 day old are not considered to have been published recently"""
+        self.old_poll = Poll.objects.create(question="new", pub_date=timezone.now() - datetime.timedelta(days=3))
+        self.assertEqual(self.old_poll.was_published_recently(), False)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_1_day_old_polls_were_published_recently(self):
+        """Poll exactly 1 day old are considered to have been published recently"""
+        self.old_poll = Poll.objects.create(question="new", pub_date=timezone.now() - datetime.timedelta(days=1))
+        self.assertEqual(self.old_poll.was_published_recently(), True)
