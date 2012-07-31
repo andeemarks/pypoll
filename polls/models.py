a894d322f82e11367b810bc2653f678aaff9b92e
Andy Marks <amarks@thoughtworks.com>
2012-07-31 08:56:42 +0800

    Update polls/models.py

--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,7 @@ class Poll(models.Model):
       return self.question
 
     def was_published_recently(self):
-      return self.pub_date > timezone.now() - datetime.timedelta(days=1)
+      return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
